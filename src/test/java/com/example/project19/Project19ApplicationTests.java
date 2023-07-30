1c20bcc1a33524216f1438848b821d83d799585d
asrytikov <asrytikov@gmail.com>
2023-07-30 21:06:27 +0400

    Initial commit

---
+++ src/test/java/com/example/project19/Project19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
