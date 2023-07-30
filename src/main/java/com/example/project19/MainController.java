198ae0161c179dbfea46fc6ccfb864159d9615a9
asrytikov <asrytikov@gmail.com>
2023-07-30 21:06:54 +0400

    Project19

--- src/main/java/com/example/project19/MainController.java
+++ src/main/java/com/example/project19/MainController.java
@@ -1,2 +1,26 @@
-package com.example.project19;public class MainController {
+package com.example.project19;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+/*    @RequestMapping("/home")
+    public String home(@RequestParam(required = false) String name, @RequestParam(required = false) String color, Model model){
+        model.addAttribute("username", name);
+        model.addAttribute("color", color);
+        return "home.html";
+    }*/
+@RequestMapping("/home/{color}")
+public String home(@PathVariable String color, Model model){
+    model.addAttribute("username", "Ivan");
+    model.addAttribute("color", color);
+    return "home.html";
+}
+
+
 }
--- src/main/java/com/example/project19/Project19Application.java
+++ src/main/java/com/example/project19/Project19Application.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Project19Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Project19Application.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(Project19Application.class, args);
+    }
 
 }
